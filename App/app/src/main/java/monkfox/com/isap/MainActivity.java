b9b8fabf1a78a08efdf508e1d7dbf5a05c95a0ec
uttamsinha97 <uttamkumar.sinha95@gmail.com>
2018-07-07 17:14:57 +0530

    regsitartion complete-uttam

--- App/app/src/main/java/monkfox/com/isap/MainActivity.java
+++ App/app/src/main/java/monkfox/com/isap/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
    // Button loginBtn= findViewById(R.id.)
     Button loginBtn ;
+    Button registrationBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent loginIntent =new Intent(getApplicationContext(),LoginFarmer.class);
                 startActivity(loginIntent);
+            }
+        });
+        registrationBtn = findViewById(R.id.regsiterfarmerbt);
+        registrationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registrationIntent = new Intent(getApplicationContext(),RegistrationActivity.class);
+                startActivity(registrationIntent);
             }
         });
         }
