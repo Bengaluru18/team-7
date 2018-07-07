829270796ccde65442a77a3d3d4f9cdaaaa61150
uttamsinha97 <uttamkumar.sinha95@gmail.com>
2018-07-08 01:06:01 +0530

    Vehciles ready-uttam

---
+++ App/app/src/main/java/monkfox/com/isap/BookNewViewBookingActivity.java
@@ -0,0 +1,13 @@
+package monkfox.com.isap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookNewViewBookingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_new_view_booking);
+    }
+}
--- App/app/src/main/java/monkfox/com/isap/LoginFarmer.java
+++ App/app/src/main/java/monkfox/com/isap/LoginFarmer.java
@@ -1,13 +1,27 @@
 package monkfox.com.isap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginFarmer extends AppCompatActivity {
 
+    Button btnlogfarmer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginfarmer);
+
+        btnlogfarmer = findViewById(R.id.btn_login_farmer);
+        btnlogfarmer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(getApplicationContext(),BookingActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
 }
