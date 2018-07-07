2c2daa4f8c622c35e3a432676902b670dd8e1c7a
Jay Sinha <razor.sinha.08@gmail.com>
2018-07-08 03:26:05 +0530

    [New] Login & Singup Implemented

--- Web/routes/index.js
+++ Web/routes/index.js
@@ -16,13 +16,56 @@ router.get("/login", function(req, res){
     sess= req.session;
     if(sess.email)
     {
-        res.redirect("/dashboard");
+        res.redirect("/");
     }
     else {
     res.render("login"); 
 }});
 
+router.post("/login", function(req, res){
+    sess= req.session;
+    if (sess.email) {
+        res.redirect("/");
+    } else {
+        user.findOne({"phone": req.body.phone}, function(err, foundUser){
+            if (err) {
+                console.log(err);
+                res.redirect("/");
+            } else {
+                if (foundUser) {
+                    res.redirect("/");
+                } else {
+                    sess.email = foundUser['phone'];
+                    sess.name = foundUser['name'];
+                    sess.role = user['role'];
+                    res.redirect("/");
+                }
+            }
+        });
+    }
+}); 
 
+router.get("/signup", function(req, res){
+    if (sess.email) {
+        res.redirect("/");
+    } else {
+        res.render("/signp");
+    }
+});
+
+router.post("/signup", function(req, res){
+    if (sess.email) {
+        res.redirect("/");
+    } else {
+        req.body.user['role'] = 4;
+        user.create(req.body.user, function(err, newUser){
+            sess.email = user['phone'];
+            sess.name = user['name'];
+            sess.role = user['role'];
+            res.redirect("/");
+        });
+    }
+});
 
 router.get("/logout",isLoggedIn, function(req, res){
     if (sess.email) {
